9f87b3c804309374f61dfabdba2d436fbdb79e70
Anjaymemek <158371771+GMFR444X@users.noreply.github.com>
2024-06-27 02:53:59 +0700

    Update dapacek.py

--- dapacek.py
+++ dapacek.py
@@ -5,12 +5,17 @@ def check_da_pa(url):
     try:
         response = requests.get(f"https://www.websiteseochecker.com/bulk-check-page-authority/?url={url}")
         soup = BeautifulSoup(response.content, 'html.parser')
-        da = soup.find('input', {'name': 'da'}).get('value')
-        pa = soup.find('input', {'name': 'pa'}).get('value')
-        return f"DA: {da}, PA: {pa}"
+        da_tag = soup.find('input', {'name': 'da'})
+        pa_tag = soup.find('input', {'name': 'pa'})
+        if da_tag and pa_tag:
+            da = da_tag.get('value')
+            pa = pa_tag.get('value')
+            return f"DA: {da}, PA: {pa}"
+        else:
+            return "Error: DA PA data not found"
     except Exception as e:
         print(f"Error checking DA PA: {e}")
-        return ""
+        return f"Error: {e}"
 
 if __name__ == "__main__":
     import sys

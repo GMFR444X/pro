06555c3dc2d17198fac30f3d06c9c38fc31fa832
Anjaymemek <158371771+GMFR444X@users.noreply.github.com>
2024-06-27 03:31:08 +0700

    Update subdomain.py

--- subdomain.py
+++ subdomain.py
@@ -3,31 +3,34 @@ from bs4 import BeautifulSoup
 
 def find_subdomains(url):
     try:
-        response = requests.get(f"https://dnsdumpster.com/")
-        soup = BeautifulSoup(response.content, 'html.parser')
-        csrfmiddlewaretoken = soup.find('input', {'name': 'csrfmiddlewaretoken'}).get('value')
-        headers = {
-            'Referer': 'https://dnsdumpster.com/',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        }
-        cookies = {
-            'csrftoken': csrfmiddlewaretoken
-        }
-        data = {
-            'csrfmiddlewaretoken': csrfmiddlewaretoken,
-            'targetip': url
-        }
         session = requests.Session()
-        response = session.post('https://dnsdumpster.com/', headers=headers, cookies=cookies, data=data)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        response = session.get("https://dnsdumpster.com")
+        soup = BeautifulSoup(response.content, "html.parser")
+        csrfmiddlewaretoken = soup.find("input", {"name": "csrfmiddlewaretoken"})["value"]
+        
+        headers = {
+            "Referer": "https://dnsdumpster.com/",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        }
+        cookies = {"csrftoken": csrfmiddlewaretoken}
+        data = {
+            "csrfmiddlewaretoken": csrfmiddlewaretoken,
+            "targetip": url
+        }
+        
+        response = session.post("https://dnsdumpster.com", headers=headers, cookies=cookies, data=data)
+        soup = BeautifulSoup(response.content, "html.parser")
+        
         subdomains = set()
-        for td in soup.find_all('td'):
+        for td in soup.find_all("td"):
             subdomain = td.text.strip()
-            if subdomain.endswith(f'.{url}'):
+            if subdomain.endswith(f".{url}"):
                 subdomains.add(subdomain)
+        
         return subdomains
     except Exception as e:
-        return f"Error finding subdomains: {e}"
+        print(f"Error finding subdomains: {e}")
+        return set()
 
 if __name__ == "__main__":
     import sys

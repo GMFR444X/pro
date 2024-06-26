6ac3441336b8ad66f9850d0ff846ed4519baa6bc
Anjaymemek <158371771+GMFR444X@users.noreply.github.com>
2024-06-27 03:13:53 +0700

    Update subdomain.py

--- subdomain.py
+++ subdomain.py
@@ -1,21 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-def find_subdomains(url):
+def find_domains_by_ip(ip):
     try:
-        response = requests.get(f"https://crt.sh/?q=%.{url}")
-        soup = BeautifulSoup(response.content, 'html.parser')
-        subdomains = set()
-        for td in soup.find_all('td'):
-            subdomain = td.text.strip()
-            if subdomain.endswith(f'.{url}'):
-                subdomains.add(subdomain)
-        return '\n'.join(subdomains)
+        response = requests.get(f"https://www.yougetsignal.com/tools/web-sites-on-web-server/php/get-web-sites-on-web-server-json.php", params={'remoteAddress': ip})
+        data = response.json()
+        domains = [site['domain'] for site in data['sites']]
+        return domains
     except Exception as e:
-        print(f"Error finding subdomains: {e}")
-        return ""
+        return f"Error finding domains by IP: {e}"
 
 if __name__ == "__main__":
     import sys
-    url = sys.argv[1]
-    print(find_subdomains(url))
+    ip = sys.argv[1]
+    print(find_domains_by_ip(ip))

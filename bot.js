8e493b5f4106e9797c92d8971ceeaaa4165e3187
Anjaymemek <158371771+GMFR444X@users.noreply.github.com>
2024-06-29 00:59:55 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,39 +2,93 @@ const TelegramBot = require('node-telegram-bot-api');
 const { spawn } = require('child_process');
 const fs = require('fs').promises;
 const path = require('path');
+const axios = require('axios');
 
 // Replace YOUR_TELEGRAM_BOT_TOKEN with your bot's token
 const token = '6674838409:AAHLkaUy93k648M8FlvlhBddJLD0NgfzYd0';
 const bot = new TelegramBot(token, { polling: true });
 
+// Function to read the progress from the progress file
+const readProgress = async (progressFile) => {
+  try {
+    const progress = await fs.readFile(progressFile, 'utf-8');
+    return parseInt(progress.trim(), 10);
+  } catch (err) {
+    return 0;
+  }
+};
+
+// Function to update progress
+const updateProgress = async (chatId, messageId, progressFile) => {
+  let progress = await readProgress(progressFile);
+  if (progress >= 100) {
+    progress = 100;
+  }
+  const options = {
+    reply_markup: {
+      inline_keyboard: [[{ text: `Proses ${progress}%`, callback_data: 'noop' }]]
+    },
+    parse_mode: 'Markdown'
+  };
+  await bot.editMessageText(`*Proses Cek Tunggu Dulu Bray*`, { chat_id: chatId, message_id: messageId, ...options });
+};
+
 // Helper function to run a Python script and handle its output
-const runPythonScript = (script, args, chatId, outputFiles) => {
-  bot.sendMessage(chatId, '*Proses Cek Tunggu Dulu Bray*', { parse_mode: 'Markdown' });
+const runPythonScript = (script, args, chatId, outputFiles, progressFile) => {
+  bot.sendMessage(chatId, '*Proses Cek Tunggu Dulu Bray*', { parse_mode: 'Markdown' }).then(async (msg) => {
+    const messageId = msg.message_id;
+    updateProgress(chatId, messageId, progressFile);
 
-  const pythonProcess = spawn('python3', [script, ...args]);
+    const pythonProcess = spawn('python3', [script, ...args]);
 
-  pythonProcess.on('close', async (code) => {
-    let resultsSent = false;
-
-    for (const file of outputFiles) {
-      try {
-        const filePath = path.resolve(file);
-        const stats = await fs.stat(filePath);
-
-        if (stats.size > 0) {
-          await bot.sendDocument(chatId, filePath);
-          resultsSent = true;
-        }
-      } catch (err) {
-        // Ignore file not found or empty file errors
+    pythonProcess.stdout.on('data', async (data) => {
+      const progress = parseInt(data.toString().trim(), 10);
+      if (!isNaN(progress) && progress >= 0 && progress <= 100) {
+        await updateProgress(chatId, messageId, progressFile);
       }
-    }
+    });
 
-    if (!resultsSent) {
-      bot.sendMessage(chatId, '*Ga ada Hasil Burik*', { parse_mode: 'Markdown' });
-    }
+    pythonProcess.on('close', async (code) => {
+      let resultsSent = false;
+
+      for (const file of outputFiles) {
+        try {
+          const filePath = path.resolve(file);
+          const stats = await fs.stat(filePath);
+
+          if (stats.size > 0) {
+            await bot.sendDocument(chatId, filePath);
+            resultsSent = true;
+          }
+        } catch (err) {
+          // Ignore file not found or empty file errors
+        }
+      }
+
+      if (!resultsSent) {
+        bot.sendMessage(chatId, '*Ga ada Hasil Burik*', { parse_mode: 'Markdown' });
+      }
+    });
   });
 };
+
+// Handle /start command
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const options = {
+    reply_markup: {
+      keyboard: [
+        ['Menu Exploit'],
+        ['/wpcek', '/cpcek', '/shellcek'],
+        ['Menu Lainnya']
+        ['/ai'],
+      ],
+      resize_keyboard: true,
+      one_time_keyboard: true
+    }
+  };
+  bot.sendMessage(chatId, 'Pilih menu:', options);
+});
 
 // Handle /wpcek command
 bot.onText(/\/wpcek/, (msg) => {
@@ -48,7 +102,7 @@ bot.onText(/\/wpcek/, (msg) => {
     const filePath = await bot.downloadFile(fileId, './');
     runPythonScript('wpcek.py', [filePath, '20'], chatId, [
       'loginSuccess.txt', 'wpfilemanager.txt', 'wptheme.txt', 'page.txt', 'plugin-install.txt'
-    ]); // Assuming thread_count is 10
+    ], 'progress.txt'); // Assuming thread_count is 10
   });
 });
 
@@ -62,7 +116,7 @@ bot.onText(/\/cpcek/, (msg) => {
     const fileId = msg.document.file_id;
 
     const filePath = await bot.downloadFile(fileId, './');
-    runPythonScript('cp.py', [filePath, '20'], chatId, ['good.txt']); // Assuming thread_count is 10
+    runPythonScript('cp.py', [filePath, '20'], chatId, ['good.txt'], 'progress.txt'); // Assuming thread_count is 10
   });
 });
 
@@ -76,8 +130,30 @@ bot.onText(/\/shellcek/, (msg) => {
     const fileId = msg.document.file_id;
 
     const filePath = await bot.downloadFile(fileId, './');
-    runPythonScript('shell.py', [filePath, '20'], chatId, ['goodshell.txt']); // Assuming thread_count is 10
+    runPythonScript('shell.py', [filePath, '20'], chatId, ['goodshell.txt'], 'shell_progress.txt'); // Assuming thread_count is 10
   });
+});
+
+// Handle /ai command
+bot.onText(/\/ai/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  const pesan = match.input.split(' ')[1];
+
+  if (!pesan) {
+    bot.sendMessage(chatId, 'Iya Selamat Datang Di bot @AnakManis1 Saya Adalah ChatGPT Salken👻 ', { reply_to_message_id: msg.message_id });
+    return;
+  }
+
+  try {
+    bot.sendChatAction(chatId, 'typing');
+    const response = await axios.get(`https://chatgpt.apinepdev.workers.dev/?question=${encodeURIComponent(pesan)}`);
+    const jawaban = response.data.answer;
+
+    bot.sendMessage(chatId, jawaban, { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
+  } catch (error) {
+    console.error('Error:', error);
+    bot.sendMessage(chatId, 'Maaf, terjadi kesalahan.');
+  }
 });
 
 // Start the bot

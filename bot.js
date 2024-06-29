18c1cb5e1e064229e0bfd60cd3dd56b793750fff
Anjaymemek <158371771+GMFR444X@users.noreply.github.com>
2024-06-29 12:30:04 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,58 +8,41 @@ const axios = require('axios');
 const token = '6674838409:AAHLkaUy93k648M8FlvlhBddJLD0NgfzYd0';
 const bot = new TelegramBot(token, { polling: true });
 
-// Kode dari tictactoe.py
-const {
-    game_grid,
-    empty_game_grid,
-    unpacking_values,
-    check_win,
-    bot_ai,
-} = require('./tictactoe');
+// Fungsi untuk menjalankan skrip Python dan menangani outputnya
+const runPythonScript = (script, args, chatId, outputFiles) => {
+  bot.sendMessage(chatId, '*Proses Cek Tunggu Dulu Bray*', { parse_mode: 'Markdown' });
 
-// Penyimpanan status permainan Tic-Tac-Toe
-const ticTacToeGames = new Map();
+  const pythonProcess = spawn('python3', [script, ...args]);
 
-// Fungsi untuk menjalankan skrip Python
-function runPythonScript(scriptName, args, chatId, resultFiles) {
-    const pythonProcess = spawn('python3', [scriptName, ...args]);
+  pythonProcess.on('close', async (code) => {
+    let resultsSent = false;
 
-    // Tampilkan pesan "Proses Cek Tunggu Dulu Bray"
-    bot.sendMessage(chatId, 'Proses Cek Tunggu Dulu Bray...');
+    for (const file of outputFiles) {
+      try {
+        const filePath = path.resolve(file);
+        const stats = await fs.stat(filePath);
 
-    pythonProcess.stdout.on('data', async (data) => {
-        const message = data.toString('utf8').trim();
-        bot.sendMessage(chatId, message);
-    });
-
-    pythonProcess.stderr.on('data', (data) => {
-        console.error(`Error: ${data}`);
-        bot.sendMessage(chatId, 'Terjadi kesalahan dalam menjalankan skrip Python.');
-    });
-
-    pythonProcess.on('close', async (code) => {
-        if (code === 0) {
-            // Kirim file hasil jika ada
-            for (const resultFile of resultFiles) {
-                try {
-                    const filePath = path.join(__dirname, resultFile);
-                    const fileData = await fs.readFile(filePath, 'utf8');
-                    bot.sendMessage(chatId, `Hasil ${resultFile}:\n${fileData}`);
-                    await fs.unlink(filePath); // Hapus file setelah dikirim
-                } catch (error) {
-                    console.error('Error sending file:', error);
-                }
-            }
-        } else {
-            bot.sendMessage(chatId, 'Skrip Python tidak berhasil dijalankan.');
+        if (stats.size > 0) {
+          await bot.sendDocument(chatId, filePath);
+          resultsSent = true;
+          // Hapus file setelah mengirim
+          await fs.unlink(filePath);
         }
-    });
-}
+      } catch (err) {
+        // Abaikan jika file tidak ditemukan atau kosong
+      }
+    }
+
+    if (!resultsSent) {
+      bot.sendMessage(chatId, '*Ga ada Hasil Burik*', { parse_mode: 'Markdown' });
+    }
+  });
+};
 
 // Menangani perintah /start
 bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-    const message = `
+  const chatId = msg.chat.id;
+  const message = `
 Pilih menu:
 
    • Menu Exploit
@@ -69,167 +52,78 @@ Pilih menu:
    • Menu Lainnya
    - /ai <pesan>
    - /simi <pesan>
-   • Menu Game
-   - /tictactoe
   `;
-    bot.sendMessage(chatId, message, { parse_mode: 'Markdown' });
+  bot.sendMessage(chatId, message, { parse_mode: 'Markdown' });
 });
 
 // Menangani perintah /wpcek
 bot.onText(/\/wpcek/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Please send me the file.');
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Please send me the file.');
 
-    bot.once('document', async (msg) => {
-        const fileId = msg.document.file_id;
-        const filePath = await bot.downloadFile(fileId, './');
-        runPythonScript('wpcek.py', [filePath, '10'], chatId, [
-            'loginSuccess.txt', 'wpfilemanager.txt', 'wptheme.txt', 'page.txt', 'plugin-install.txt'
-        ]);
-    });
+  bot.once('document', async (msg) => {
+    const fileId = msg.document.file_id;
+    const filePath = await bot.downloadFile(fileId, './');
+    runPythonScript('wpcek.py', [filePath, '10'], chatId, [
+      'loginSuccess.txt', 'wpfilemanager.txt', 'wptheme.txt', 'page.txt', 'plugin-install.txt'
+    ]);
+  });
 });
 
 // Menangani perintah /cpcek
 bot.onText(/\/cpcek/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Please send me the file.');
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Please send me the file.');
 
-    bot.once('document', async (msg) => {
-        const fileId = msg.document.file_id;
-        const filePath = await bot.downloadFile(fileId, './');
-        runPythonScript('cp.py', [filePath, '10'], chatId, ['good.txt']);
-    });
+  bot.once('document', async (msg) => {
+    const fileId = msg.document.file_id;
+    const filePath = await bot.downloadFile(fileId, './');
+    runPythonScript('cp.py', [filePath, '10'], chatId, ['good.txt']);
+  });
 });
 
 // Menangani perintah /shellcek
 bot.onText(/\/shellcek/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Please send me the file.');
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Please send me the file.');
 
-    bot.once('document', async (msg) => {
-        const fileId = msg.document.file_id;
-        const filePath = await bot.downloadFile(fileId, './');
-        runPythonScript('shell.py', [filePath, '10'], chatId, ['goodshell.txt']);
-    });
+  bot.once('document', async (msg) => {
+    const fileId = msg.document.file_id;
+    const filePath = await bot.downloadFile(fileId, './');
+    runPythonScript('shell.py', [filePath, '10'], chatId, ['goodshell.txt']);
+  });
 });
 
 // Menangani perintah /ai
 bot.onText(/\/ai (.+)/, async (msg, match) => {
-    const chatId = msg.chat.id;
-    const pesan = match[1];
+  const chatId = msg.chat.id;
+  const pesan = match[1];
 
-    try {
-        bot.sendChatAction(chatId, 'typing');
-        const response = await axios.get(`https://chatgpt.apinepdev.workers.dev/?question=${encodeURIComponent(pesan)}`);
-        const jawaban = response.data.answer;
-        bot.sendMessage(chatId, jawaban, { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
-    } catch (error) {
-        console.error('Error:', error);
-        bot.sendMessage(chatId, 'Maaf, terjadi kesalahan.');
-    }
+  try {
+    bot.sendChatAction(chatId, 'typing');
+    const response = await axios.get(`https://chatgpt.apinepdev.workers.dev/?question=${encodeURIComponent(pesan)}`);
+    const jawaban = response.data.answer;
+    bot.sendMessage(chatId, jawaban, { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
+  } catch (error) {
+    console.error('Error:', error);
+    bot.sendMessage(chatId, 'Maaf, terjadi kesalahan.');
+  }
 });
 
 // Menangani perintah /simi
 bot.onText(/\/simi (.+)/, async (msg, match) => {
-    const chatId = msg.chat.id;
-    const pesan = match[1];
+  const chatId = msg.chat.id;
+  const pesan = match[1];
 
-    try {
-        bot.sendChatAction(chatId, 'typing');
-        const response = await axios.get(`https://simsimi.site/api/v2/?mode=talk&lang=en&message=${encodeURIComponent(pesan)}&filter=true`);
-        const jawaban = response.data.success ? response.data.success : "Maaf, terjadi kesalahan.";
-        bot.sendMessage(chatId, jawaban, { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
-    } catch (error) {
-        console.error('Error:', error);
-        bot.sendMessage(chatId, 'Maaf, terjadi kesalahan.');
-    }
-});
-
-// Menangani perintah /tictactoe
-bot.onText(/\/tictactoe/, (msg) => {
-    const chatId = msg.chat.id;
-    const gameData = {
-        values_dict: {
-            '1': ' ', '2': ' ', '3': ' ',
-            '4': ' ', '5': ' ', '6': ' ',
-            '7': ' ', '8': ' ', '9': ' '
-        },
-        all_moves_set: new Set(['1', '2', '3', '4', '5', '6', '7', '8', '9'])
-    };
-    ticTacToeGames.set(chatId, gameData);
-
-    bot.sendMessage(chatId, 'Permainan Tic-Tac-Toe dimulai!', {
-        reply_markup: game_grid(gameData.values_dict, gameData.all_moves_set)
-    });
-});
-
-// Menangani callback dari game Tic-Tac-Toe
-bot.on('callback_query', async (callbackQuery) => {
-    const chatId = callbackQuery.message.chat.id;
-    const gameId = chatId;
-    const gameData = ticTacToeGames.get(gameId);
-
-    if (!gameData) {
-        bot.answerCallbackQuery(callbackQuery.id, { text: 'Permainan tidak ditemukan.' });
-        return;
-    }
-
-    const { values_dict, all_moves_set } = gameData;
-    const { data } = callbackQuery;
-
-    if (data === 'none') {
-        bot.answerCallbackQuery(callbackQuery.id, { text: 'Pilih kotak yang valid.' });
-        return;
-    }
-
-    const [gameIdStr, move, gameValue] = data.split('-');
-
-    if (gameIdStr !== '1') {
-        bot.answerCallbackQuery(callbackQuery.id, { text: 'Pesan tidak valid.' });
-        return;
-    }
-
-    values_dict[move] = '❌';
-    all_moves_set.delete(move);
-
-    const [isGameFinished, gameResult] = check_win(values_dict);
-
-    if (isGameFinished) {
-        await bot.editMessageText(`Permainan berakhir. ${gameResult}`, {
-            chat_id: chatId,
-            message_id: callbackQuery.message.message_id
-        });
-        ticTacToeGames.delete(gameId);
-        return;
-    }
-
-    bot.editMessageReplyMarkup(game_grid(values_dict, all_moves_set), {
-        chat_id: chatId,
-        message_id: callbackQuery.message.message_id
-    });
-
-    setTimeout(async () => {
-        const botMove = bot_ai(values_dict, all_moves_set);
-        values_dict[botMove] = '⭕';
-        all_moves_set.delete(botMove);
-
-        const [isGameFinished, gameResult] = check_win(values_dict);
-
-        if (isGameFinished) {
-            await bot.editMessageText(`Permainan berakhir. ${gameResult}`, {
-                chat_id: chatId,
-                message_id: callbackQuery.message.message_id
-            });
-            ticTacToeGames.delete(gameId);
-            return;
-        }
-
-        bot.editMessageReplyMarkup(game_grid(values_dict, all_moves_set), {
-            chat_id: chatId,
-            message_id: callbackQuery.message.message_id
-        });
-
-    }, 700);
+  try {
+    bot.sendChatAction(chatId, 'typing');
+    const response = await axios.get(`https://simsimi.site/api/v2/?mode=talk&lang=en&message=${encodeURIComponent(pesan)}&filter=true`);
+    const jawaban = response.data.success ? response.data.success : "Maaf, terjadi kesalahan.";
+    bot.sendMessage(chatId, jawaban, { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
+  } catch (error) {
+    console.error('Error:', error);
+    bot.sendMessage(chatId, 'Maaf, terjadi kesalahan.');
+  }
 });
 
 // Menjalankan bot
